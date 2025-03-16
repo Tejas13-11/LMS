046c4b9c5347684f98653ffa13728d47d5b320aa
Your Name <your.email@example.com>
2025-03-17 01:39:38 +0530

    Add course, user, and educator routes with associated models and middleware

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
-import express from 'express';
+import express, { json } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './configs/mongodb.js';
-import { clerkWebhooks } from './controllers/webhooks.js';
+import { clerkWebhooks, stripeWebhooks } from './controllers/webhooks.js';
+import educatorRouter from './routes/educatorRoutes.js';
+import { clerkMiddleware } from '@clerk/express';
+import connectCloudinary from './configs/cloudinary.js';
+import courseRouter from './routes/courseRoute.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -11,17 +16,22 @@ const app = express();
 // Middleware to parse JSON payloads
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
 
 app.get('/', (req, res) => {
   res.send("API Working");
 });
 
 app.post('/clerk', express.json(), clerkWebhooks);
-
+app.use('/api/educator', express.json(), educatorRouter);
+app.use('/api/course', express.json(), courseRouter);
+app.use('/api/user', express.json(), userRouter);
+app.post('/stripe',express.raw({'application/json'}),stripeWebhooks)
 const startServer = async () => {
   try {
     // Connect to database
     await connectDB();
+    await connectCloudinary();
 
     const PORT = process.env.PORT || 5000;
 
